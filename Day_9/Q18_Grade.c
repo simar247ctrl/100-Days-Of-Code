564471e7e90f4abd57251bf5bb9067b345784df0
simar247ctrl <simar2471@gmail.com>
2026-08-19 07:33:16 +0530

    Add files via upload
    Q17 (Conditional Statements)
    Write a program to find the roots of a quadratic equation and categorize them.
    Q18 (Conditional Statements)
    Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
    90-100: Grade A 
    80-89: Grade B 
    70-79: Grade C 
    60-69: Grade D 
    below 60: Grade F.

---
+++ Day_9/Q18_Grade.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+    if(marks <0 || marks >100){
+        printf("Invalid marks! ");
+    }
+
+    else if(marks >= 90 && marks <= 100){
+        printf("Grade A");
+    }
+    else if(marks >= 80){
+        printf("Grade B");
+    }
+    else if(marks >= 70){
+        printf("Grade C");
+    }
+    else if(marks >= 60){
+        printf("Grade D");
+    }
+    else{
+        printf("Grade F");
+    }
+    
+
+    return 0;
+}
