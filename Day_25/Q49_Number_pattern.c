0dab9aaf33a9f7fe590db29d769656cdc3efd827
simar247ctrl <simar2471@gmail.com>
2026-09-03 21:48:55 +0530

    Add files via upload
    Q49 (Nested Loops without Arrays/Strings) Write a program to print the following pattern:
    5
    45
    345
    2345
    12345
    Q50 (Nested Loops without Arrays/Strings) Write a program to print the following pattern:
    *****
     ****
      ***
       **
        *

---
+++ Day_25/Q49_Number_pattern.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+   
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(int i = n; i >= 1; i-- ){
+      for(int j = i; j <= n ; j++){
+        printf("%d", j);
+
+      }
+      printf("\n");
+
+    }
+  
+
+    return 0;
+}
